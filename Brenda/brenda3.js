bf28b31569efdae7add74484ffe204db9d0456cf
Gabriella Rodrigues <gabriella.r.r.2106@gmail.com>
2022-05-29 16:22:31 -0300

    ajustes finais brenda e add fase 1 marcos

--- Brenda/brenda3.js
+++ Brenda/brenda3.js
@@ -4,7 +4,7 @@ function escolha (){
     {var decisao= prompt ("Seguir o suspeito? Digite 1 para 'Sim' e 2 para 'Não'.")
     if (decisao == 1) {
         alert ("Ao fazer boas escolhas você evitou um assassinato no banheiro e foi promovida. Parabéns!");
-        return window.location.href="vencedor.html";
+        return window.location.href="../vencedor.html";
     }
     else if (decisao == 2) {
         alert ("Uma pessoa morre no banheiro e você não vê, perdendo assim o seu cargo na polícia.");
