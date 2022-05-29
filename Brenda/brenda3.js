7cf68714030d7bfabd2f0026992267b1f6aeea80
Gabriella Rodrigues <gabriella.r.r.2106@gmail.com>
2022-05-29 01:39:26 -0300

    add fase 3 brenda

---
+++ Brenda/brenda3.js
@@ -0,0 +1,17 @@
+function escolha (){
+    
+    while (true)
+    {var decisao= prompt ("Seguir o suspeito? Digite 1 para 'Sim' e 2 para 'Não'.")
+    if (decisao == 1) {
+        alert ("Ao fazer boas escolhas você evitou um assassinato no banheiro e foi promovida. Parabéns!");
+        return window.location.href="vencedor.html";
+    }
+    else if (decisao == 2) {
+        alert ("Uma pessoa morre no banheiro e você não vê, perdendo assim o seu cargo na polícia.");
+        return window.location.href="../gameover.html";
+        break
+    }
+        alert ("Digite uma opção válida.")
+    }
+}
+escolha()
